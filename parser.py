2eba1e46b5cd7be8f83aec0489d2852e0c2e2f76
Arsen Losenko <arsenlosenko@gmail.com>
2018-03-10 21:42:19 +0200

    Refactored to use aiohttp

--- parser.py
+++ parser.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python3
 
-import requests
-import datetime
+import aiohttp
+import asyncio
+import async_timeout
+
+CITY_SEARCH_URL = 'https://booking.uz.gov.ua/train_search/station/'
+TRAIN_DATA_URL = 'https://booking.uz.gov.ua/train_search/'
+WAGON_DATA_URL = 'https://booking.uz.gov.ua/train_wagon/'
 
 data_dict ={ 
         'from': 'Вінниця',
         'to':'Київ',
-        'date':'2018-03-10',
+        'date':'2018-03-12',
         'time':'00:00',
         'get_tpl':1
         }
 
-def get_city_code(city_name):
-    city_search_url = 'https://booking.uz.gov.ua/train_search/station/'
-    cities = requests.get(city_search_url,params={'term': city_name})
-    cities_json = cities.json()
-    city_data = list(filter(lambda x: x['title'] == city_name, cities_json))
+async def async_get(url, data):
+    async with aiohttp.ClientSession() as session:
+        async with async_timeout.timeout(10):
+            async with session.get(url, params=data) as resp:
+                return resp.json()
+
+async def async_post(url, data):
+    async with aiohttp.ClientSession() as session:
+        async with async_timeout.timeout(10):
+            async with session.post(url, data=data) as resp:
+                return resp.json()
+
+async def get_city_code(city_name):
+    cities_json = await async_get(CITY_SEARCH_URL, {'term': city_name})
+    city_data = list(filter(lambda x: x['title'] == city_name, await cities_json))
     city_code = city_data[0]['value']
     return city_code
 
-def retrieve_trains_data(data_dict):
-    train_data_url = 'https://booking.uz.gov.ua/train_search/'
-    from_city_code = get_city_code(data_dict['from'])
-    to_city_code = get_city_code(data_dict['to'])
-    data_dict['from'] = from_city_code
-    data_dict['to'] = to_city_code
-    train_data = requests.post(train_data_url, data=data_dict)
-    return train_data.json()
-
-def parse_train_data(data_dict):
-    train_data = retrieve_trains_data(data_dict)
+async def retrieve_trains_data(data_dict):
+    data_dict['from'] = await get_city_code(data_dict['from'])
+    data_dict['to'] = await get_city_code(data_dict['to'])
+    train_data = await async_post(TRAIN_DATA_URL, data=data_dict)
+    return await train_data
+    
+async def parse_train_data(data_dict):
+    train_data = await retrieve_trains_data(data_dict)
     trains_list = train_data['data']['list']
     for train in trains_list:
         train_data_dict = {
@@ -42,18 +54,17 @@ def parse_train_data(data_dict):
                 'wagon_class': "Д",
                 'cached_scheme[]': "П01"
             }
-        retrieve_wagon_data(train_data_dict)
+        await retrieve_wagon_data(train_data_dict)
 
-def retrieve_wagon_data(train_data_dict):
-    wagon_data_url = 'https://booking.uz.gov.ua/train_wagon/'
-    wagon_data = requests.post(wagon_data_url, data=train_data_dict)
-    wagon_data_json = wagon_data.json()
+async def retrieve_wagon_data(train_data_dict):
+    wagon_data = await async_post(WAGON_DATA_URL, data=train_data_dict)
+    wagon_data_json = await wagon_data
     if 'error' in wagon_data_json.keys() and train_data_dict['wagon_num'] <= 15:
         train_data_dict['wagon_num'] += 1
-        retrieve_wagon_data(train_data_dict)
+        await retrieve_wagon_data(train_data_dict)
     print(wagon_data_json)
 
 
 if __name__ == "__main__":
-    parse_train_data(data_dict)
-
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(parse_train_data(data_dict))
